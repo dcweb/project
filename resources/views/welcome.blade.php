e7e959329714e0de7823bd98e54d0b727abcb241
pieterheyns <phe@groupdc.be>
2017-05-30 14:41:30 +0200

    css background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #ff0011;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
