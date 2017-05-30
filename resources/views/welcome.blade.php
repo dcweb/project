dc4911613c9e0a47f35f5026d983c76b2cae9c3d
BartReymenants <bre@groupdc.be>
2017-05-30 15:34:36 +0200

    r82 edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Git Master
                 </div>
 
                 <div class="links">
